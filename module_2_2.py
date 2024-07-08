255341aeebb24fba1ee58c7dac07e900b7d651d2
VeraK87 <168467151+VeraK87@users.noreply.github.com>
2024-07-08 23:12:01 +0300

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -5,5 +5,6 @@ if first == second == third:
     print(3)
 elif first == second or second != third:
     print(2)
-else: first != second != third and first != third
+else:
+    var = first != second != third and first != third
 print(0)
