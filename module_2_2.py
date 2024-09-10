68aae7ce82521e371e12779b48df1baa8f450f64
VeraK87 <168467151+VeraK87@users.noreply.github.com>
2024-09-10 22:44:54 +0300

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -1,6 +1,6 @@
-first = "3"
-second = "4"
-third = "10"
+first = int(input(9))
+second = int(input(4))
+third = int(input(10))
 if first == second == third:
     print(3)
 elif first == second or second != third:
