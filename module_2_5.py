cad2597789b0d2b4b0e1fa7532494fc1527f0145
VeraK87 <168467151+VeraK87@users.noreply.github.com>
2024-10-15 23:46:49 +0300

    Create module_2_5.py

---
+++ module_2_5.py
@@ -0,0 +1,8 @@
+def get_matrix(n,m,value):
+    matrix = []
+    for i in range(n):
+        matrix = []
+        for j in range(m):
+            value = []
+            print(matrix)
+            return matrix 
